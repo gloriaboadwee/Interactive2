a497f24277d38387d3d5953c87f61e57b27f790d
gloria <Gloriab@kiwikiwikiwi.local>
2018-09-25 18:45:05 -0700

    scale

---
+++ cssFont/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    // store code for your letters in variables
+
+    // check when the user clicks
+  	$(document).click(function(e){
+       {
+        background: yellow;
+    }
+
+
+
+
+
+});
