dac061562fddc8dc09b1c6bbaf5d7c17d15bf15d
gloria <Gloriab@kiwikiwikiwi.local>
2018-09-27 18:48:30 -0700

    m

--- FontSite/js/script.js
+++ FontSite/js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
     // store code for your letters in variables
+
+    $( function() {
+      $( ".draggable" ).draggable();
+    } );
+
+    $('.draggable').click(function(){
+      $(this).clone().prependTo(this).parent();
+    });
+
 });
