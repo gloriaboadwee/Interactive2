062d2b127b084ec2c7c55b71690b35689def873f
gloria <Gloriab@kiwikiwikiwi.local>
2018-10-10 23:54:32 -0700

    x

--- 50results/js/script.js
+++ 50results/js/script.js
@@ -17,6 +17,9 @@ $(document).ready(function(){
 		$(this).toggleClass('circle');
 	});
 
+  $('#button-24').click(function() {
+		$(this).toggleClass('shape');
+	});
 
 	// check when the mouse moves
 	$('#button-6').mousemove(function(e){
@@ -39,22 +42,6 @@ $(document).ready(function(){
   $('#button-19').click(function(){
       alert("I am a button!");
 });
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("button-21").style.display = "block";
-    } else {
-        document.getElementById("button21").style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
 
 
 
